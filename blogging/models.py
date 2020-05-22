6c51ee4bb0554d0e6ab969dd41cbfe41809ebe05
Le Vinh <Vinh.Le@elektrobit.com>
2020-05-22 13:59:36 -0700

    Applied black.

--- blogging/models.py
+++ blogging/models.py
@@ -1,4 +1,4 @@
-from django.db import models 
+from django.db import models
 from django.contrib.auth.models import User
 
 
@@ -9,18 +9,18 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
-    
+
     def __str__(self):
         return self.title
 
 
 class Category(models.Model):
     name = models.CharField(max_length=128)
-    description = models.TextField(blank = True)
-    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
+    description = models.TextField(blank=True)
+    posts = models.ManyToManyField(Post, blank=True, related_name="categories")
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = "Categories"
--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,9 @@ from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    path('polling/', include('polling.urls')),
-    path('', include('blogging.urls')),
-    path('admin/', admin.site.urls),
-    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
-    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
+    path("polling/", include("polling.urls")),
+    path("", include("blogging.urls")),
+    path("admin/", admin.site.urls),
+    path("login/", LoginView.as_view(template_name="login.html"), name="login"),
+    path("logout/", LogoutView.as_view(next_page="/"), name="logout"),
 ]
-
